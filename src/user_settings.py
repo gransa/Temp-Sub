94103611964b4fb094469529dbef57b8f54fa8a7
GB <129722511+gransa@users.noreply.github.com>
2025-07-17 22:05:51 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -4,7 +4,7 @@
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
     "https://wy.flylink.cyou/api/v1/client/subscribe?token=d984bede85ab3f30dd029d49027630e6",
-    "https://loopycloudcfjiasucdn.xx.kg/api/838286f3e05dd1dd2c41446489aca566",
+    "https://loopycloudcfjiasucdn.xx.kg/api/d22cc5df9c2f4292ecfc23e1bbb27c6d",
     "https://ne997.688997.xyz/api/v1/client/subscribe?token=157b7041c116259e45fa7c82b0659aff",
     # Add more URLs here if you want to include additional sources.
 ]
