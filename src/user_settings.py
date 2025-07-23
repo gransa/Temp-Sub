cfe3d7963408dcd95270d9a50d09cddf89034945
GB <129722511+gransa@users.noreply.github.com>
2025-07-23 11:19:28 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -3,8 +3,6 @@
 # List of source URLs to fetch proxy configurations from.
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
-    "https://wy.flylink.cyou/api/v1/client/subscribe?token=d984bede85ab3f30dd029d49027630e6",
-    "https://loopycloudcfjiasucdn.xx.kg/api/ca692ddffaf7a69b9c15e9be536a457f",
     "https://qqyz.yinheyz.cfd/api/v1/client/subscribe?token=2b0e60c7f59e0b319c4444f1345a674d",
     # Add more URLs here if you want to include additional sources.
 ]
