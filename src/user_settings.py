c29900df7e0c7c1c1021031f4c1af76c2967a398
GB <129722511+gransa@users.noreply.github.com>
2025-08-11 14:21:02 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -3,7 +3,7 @@
 # List of source URLs to fetch proxy configurations from.
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
-    "https://wanmeiwl3.xyz/gywl/c359faa6381ac483208d3569a4112f97",
+    "",
     # Add more URLs here if you want to include additional sources.
 ]
 
